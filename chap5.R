0195391d741ae81fece20036ff2b8280a8d9c5d3
taekjoo <taekjoo@gmail.com>
2014-09-17 09:26:55 +0900

    5.5.2

--- chap5.R
+++ chap5.R
@@ -1,21 +1,23 @@
-mushroom <- read.csv(file.choose(), header = T)
-p1 = 4488/(4488+3928)
-p2 = 3928/(4488+3928)
-entropyMushroom <- -(p1*log2(p1)+p2*log2(p2))
-
+install.packages("car")
+install.packages("pastecs")
+install.packages("psych")
 install.packages("outliers")
+
+library(car)
+library(pastecs)
+library(psych)
 library(outliers)
+library(ggplot2)
 
 dlf<-read.delim(file.choose(), header = T)
-day1 <- outlier(x = dlf$day1, opposite = F, logical = F)
+out1 <- outlier(x = dlf$day1, opposite = F, logical = F)
 out2 <- outlier(x = dlf$day2, opposite = F, logical = F)
 out3 <- outlier(x = dlf$day3,opposite = F, logical = F)
 
-dlf$day1[dlf$day1 == 20.02] <- NA
-dlf$day2[dlf$day2 == 3.45] <- NA
-dlf$day2[dlf$day2 == 3.41] <- NA
+dlf$day1[dlf$day1 == out1] <- NA
+dlf$day2[dlf$day2 == out2] <- NA
+dlf$day3[dlf$day3 == out3] <- NA
 
-library(ggplot2)
 qplot(dlf$day1)
 
 hist.day1 <- ggplot(data = dlf, aes(day1)) + theme(legend.position = "none") + geom_histogram(aes(y=..density..), color = "black", fill = "white") + labs(x = "Hygiene score on day 1", y = "Density")
@@ -36,3 +38,12 @@ qqplot.day2
 
 qqplot.day3 <- qplot(sample = dlf$day3, stat = "qq")
 qqplot.day3
+
+describe(cbind(dlf$day1, dlf$day2, dlf$day3))
+stat.desc(dlf$day1, basic = F, norm = T)
+
+describe(cbind(dlf$day1, dlf$day2, dlf$day3))
+stat.desc(cbind(dlf$day1, dlf$day2, dlf$day3), basic = F, norm = T)
+
+describe(dlf[,c("day1", "day2", "day3")])
+stat.desc(dlf[, c("day1","day2", "day3")], basic = F, norm = T)
